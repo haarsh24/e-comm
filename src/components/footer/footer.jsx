05da4f2a4bc3441ae27751aeb5d5558fdb76a14a
Kumar Harsh <kumarharsh241998@gmail.com>
2022-04-01 19:11:53 +0530

    fix: footer link

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -7,7 +7,7 @@ const Footer = () =>{
         <footer className="footer">
         <h2>
             Designed and Developed by
-            <Link className="name-tag h2" to="https://kumarharsh.netlify.app/"> harsh </Link>
+            <a className="name-tag h2" href="https://kumarharsh.netlify.app/"> harsh </a>
         </h2>
     </footer>
         </>
